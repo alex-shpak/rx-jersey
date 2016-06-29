85638905a2ff4e201c234d887fc3c3e25f4b57c2
Alex Shpak <alex-shpak@users.noreply.github.com>
2016-06-29 22:02:25 +0200

    Initial commit

---
+++ jersey-feature/src/main/java/net/winterly/rx/jersey/feature/RxJerseyFeature.java
@@ -0,0 +1,24 @@
+package net.winterly.rx.jersey.feature;
+
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.spi.internal.ResourceMethodInvocationHandlerProvider;
+
+import javax.inject.Singleton;
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.FeatureContext;
+
+public class RxJerseyFeature implements Feature {
+
+    @Override
+    public boolean configure(FeatureContext context) {
+        context.register(RxBodyWriter.class);
+        context.register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(RxInvocationHandlerProvider.class).to(ResourceMethodInvocationHandlerProvider.class).in(Singleton.class);
+            }
+        });
+        return true;
+    }
+
+}
