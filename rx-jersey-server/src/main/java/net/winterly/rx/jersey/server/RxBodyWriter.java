c50a8773d077a4eef8cb59c5534d936a674e06e0
Alex Shpak <alex-shpak@users.noreply.github.com>
2017-04-02 13:02:51 +0200

    #2: Set higher priority for RxBodyWriter

--- rx-jersey-server/src/main/java/net/winterly/rx/jersey/server/RxBodyWriter.java
+++ rx-jersey-server/src/main/java/net/winterly/rx/jersey/server/RxBodyWriter.java
@@ -4,12 +4,14 @@ import net.winterly.rxjersey.server.RxGenericBodyWriter;
 import rx.Observable;
 import rx.Single;
 
+import javax.annotation.Priority;
 import javax.inject.Singleton;
 
 /**
  * MessageBodyWriter accepting {@link rx.Observable} or {@link rx.Single} and routing to write entity of generic type instead
  */
 @Singleton
+@Priority(1)
 public class RxBodyWriter extends RxGenericBodyWriter {
     public RxBodyWriter() {
         super(Observable.class, Single.class);
