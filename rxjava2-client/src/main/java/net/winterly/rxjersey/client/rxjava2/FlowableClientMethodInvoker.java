1ade10cdeabb5c7072d38ac9a481e7e557429631
coreyjboyle <corey@kansanian.com>
2020-04-21 17:56:39 -0400

    Support javax.ws.rs.core.Response values wrapped in Rx containers (#17)

--- rxjava2-client/src/main/java/net/winterly/rxjersey/client/rxjava2/FlowableClientMethodInvoker.java
+++ rxjava2-client/src/main/java/net/winterly/rxjersey/client/rxjava2/FlowableClientMethodInvoker.java
@@ -7,6 +7,7 @@ import org.glassfish.jersey.client.rx.rxjava2.RxFlowableInvoker;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.GenericType;
+import java.lang.reflect.ParameterizedType;
 import java.util.HashMap;
 import java.util.function.Function;
 
@@ -24,18 +25,42 @@ public class FlowableClientMethodInvoker implements ClientMethodInvoker<Object> 
 
     @Override
     public <T> Object method(Invocation.Builder builder, String name, GenericType<T> responseType) {
-        Flowable<T> flowable = builder.rx(RxFlowableInvoker.class).method(name, responseType);
+        GenericType<?> responseValueType = getValueTypeIfPossible(responseType);
+        Flowable<?> flowable = builder.rx(RxFlowableInvoker.class).method(name, responseValueType);
         return convert(flowable, responseType);
     }
 
     @Override
     public <T> Object method(Invocation.Builder builder, String name, Entity<?> entity, GenericType<T> responseType) {
-        Flowable<T> flowable = builder.rx(RxFlowableInvoker.class).method(name, entity, responseType);
+        GenericType<?> responseValueType = getValueTypeIfPossible(responseType);
+        Flowable<?> flowable = builder.rx(RxFlowableInvoker.class).method(name, entity, responseValueType);
         return convert(flowable, responseType);
     }
 
-    private <T> Object convert(Flowable<T> flowable, GenericType<T> responseType) {
+    private <T> Object convert(Flowable<?> flowable, GenericType<T> responseType) {
         Function<Flowable, ?> converter = converters.get(responseType.getRawType());
         return converter.apply(flowable);
+    }
+
+    /**
+     * Jersey has special handling when the invoker response type is javax.ws.rs.core.Response
+     * To maintain that behavior, use the type of the values in the Rx container (in case it is e.g. Single<Response>)
+     * @param responseType
+     * @param <T>
+     * @return
+     */
+    private <T> GenericType getValueTypeIfPossible(GenericType<T> responseType) {
+        if (isConvertibleParameterizedType(responseType)) {
+            return getContainedType((ParameterizedType) responseType.getType());
+        }
+        return responseType;
+    }
+
+    private <T> boolean isConvertibleParameterizedType(GenericType<T> type) {
+        return converters.containsKey(type.getRawType()) && type.getType() instanceof ParameterizedType;
+    }
+
+    private GenericType getContainedType(ParameterizedType type) {
+        return new GenericType(type.getActualTypeArguments()[0]);
     }
 }
